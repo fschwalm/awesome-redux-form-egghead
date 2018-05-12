99595f4cc98f1bde05b3e0fb2f160d58cebfed31
Fagner <fagnerschwalm@gmail.com>
2018-05-11 22:52:38 -0300

    Use Asynchronous Validation after Submitting a Form in redux-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import RegisterForm from "./components/RegisterForm";
+import { SubmissionError } from 'redux-form';
 
 class RegisterFormContainer extends React.Component {
   submit = values => {
+    if(['kent', 'andy', 'john', 'joel'].includes(values.username)){
+      throw new SubmissionError({
+        username: 'Username already taken'
+      })
+    }
     window.alert(JSON.stringify(values, null, 4));
   };
 
